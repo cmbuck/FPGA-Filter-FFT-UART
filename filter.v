f7072fc85ccef455c8035a593679dae1a57597ac
Christopher Buck <cmbuck@vt.edu>
2015-12-13 14:28:02 -0600

    wrote more filter.v with input port arrays

--- filter.v
+++ filter.v
@@ -21,10 +21,73 @@
 module filter(
     input clk,
     input rst,
-    input inSignal,
-    output outSignal
+    input [17:0] inSignal[0:63],
+	 input newData,
+    output reg [17:0] outSignal[0:63],
+	 output reg dataReady
     );
 
-assign outSignal = inSignal;
+reg [17:0] dataA [0:63];
+reg [17:0] dataB [0:63];
+reg state[2:0];
+reg index [5:0];
+
+wire [17:0] mult1A, mult1B, mult1P;
+
+//Instantiate Fixed Point Multipliers
+fpMultiply mult1 (
+	  .A(mult1A), 
+	  .B(mult1B), 
+	  .P(mult1P)
+	);
+
+always @(posedge clk)
+begin
+	if (rst)
+	begin
+		dataReady <= 0;
+		outSignal <= 0;
+		dataA <= 0;
+		dataB <= 0;
+	end
+	else
+	begin
+		//Regular operation
+		
+		
+		
+		case (state)
+		
+		0 : 
+		begin
+			//Idle
+			if (newData)  //Register in the new data
+			begin
+				dataReady <= 0;
+				dataA <= inSignal;
+				state <= 1; //begin 
+				index <= 0;
+			end
+		end
+			
+		1 : 
+		begin
+			//Filter
+			if (index == 63) state <= 2;
+			
+		end
+			
+		2 :
+		begin
+			//Output
+				ouSignal <= dataB;
+				dataReady <= 1;
+				state <= 0;
+		end
+		
+		endcase
+		
+	end
+end
 
 endmodule
